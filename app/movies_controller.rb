61d11c7d8a998bd22d8ccda46c4fc9387e0c0638
Iskra B <ibatistapoblete@gmail.com>
2019-07-12 01:46:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,6 +33,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m = Movie.new(args)
   end
+  m
 end
 
 def can_get_the_first_item_in_the_database

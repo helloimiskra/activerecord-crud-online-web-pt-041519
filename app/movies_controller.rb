fa653a36b4753e21cfa7849608506134f5547cfe
Iskra B <ibatistapoblete@gmail.com>
2019-07-12 02:03:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -54,11 +54,13 @@ def can_find_the_first_item_from_the_database_using_id
 end
 
 def can_find_by_multiple_attributes
+  # attributes = {title, release_date, director}
+  Movie.find_by(title: "Title", release_date: "2000", director: "Me")
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+
 end
 
 def can_find_using_where_clause_and_be_sorted
